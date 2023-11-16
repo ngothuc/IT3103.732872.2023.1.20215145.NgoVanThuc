8271e8256f6b3ec1c03826d232aa4cdb714d9f1e
Ngo Van Thuc <ngothuc0704@gmail.com>
2023-11-16 17:34:44 +0700

    Lab03

--- Lab02/AimsProject/src/Lab02/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/Lab02/DigitalVideoDisc.java
@@ -2,13 +2,17 @@ package Lab02;
 // Ngo_Van_Thuc_20215145
 public class DigitalVideoDisc {	
 	private String title;
-	public void setTitle(String title) {
-		this.title = title;
-	}
 	private String category;
 	private String director;
 	private int length;
 	private float cost;
+
+	private static int nbDigitalVideoDisc = 0;
+	private int id;
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 		
 	public String getTitle() {
 		return title;
@@ -31,10 +35,12 @@ public class DigitalVideoDisc {
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		this.id = ++nbDigitalVideoDisc;
 	}
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		this.id = ++nbDigitalVideoDisc;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -42,6 +48,7 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		this.id = ++nbDigitalVideoDisc;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -50,6 +57,7 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		this.id = ++nbDigitalVideoDisc;
 	}
 	
 }
